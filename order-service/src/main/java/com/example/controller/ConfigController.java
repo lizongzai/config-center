9466403f625accf8945e744b98cd03b0038f5c33
root <lizongzai@gmail.com>
2023-03-12 22:23:54 +0800

    V2 全套微服务流程使用指南

--- order-service/src/main/java/com/example/controller/ConfigController.java
+++ order-service/src/main/java/com/example/controller/ConfigController.java
@@ -1,6 +1,7 @@
 package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,15 +12,28 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2023/03/12 18:38
  * @since 1.0.0
  */
+@RefreshScope
 @RestController
 public class ConfigController {
 
   @Value("${name}")
   private String name;
+  @Value("${password}")
+  private String password;
 
   @GetMapping("/name")
   public String getName() {
     return name;
+  }
+
+  /**
+   * 获取解密后的密码
+   *
+   * @return
+   */
+  @GetMapping("/password")
+  public String getPassword() {
+    return password;
   }
 
 }
--- product-service/src/main/java/com/example/controller/ConfigController.java
+++ product-service/src/main/java/com/example/controller/ConfigController.java
@@ -1,6 +1,7 @@
 package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2023/03/12 18:38
  * @since 1.0.0
  */
+@RefreshScope
 @RestController
 public class ConfigController {
 
